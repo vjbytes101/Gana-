5d08c26a7cf574b3a23c248a6e032c2bf3a3bb0f
Inv1cts <hs3499@nyu.edu>
2017-10-04 12:41:03 -0400

    remember me removed and home page redirect

--- app/server/routes.js
+++ app/server/routes.js
@@ -5,18 +5,11 @@ module.exports = function(app) {
     // main login page //
     app.get('/', function(req, res) {
         // check if the user's credentials are saved in a cookie //
-        if (req.cookies.user == undefined || req.cookies.pass == undefined) {
+        if (req.session.user == null) {
+            // if user is not logged-in redirect back to login page //
             res.render('login', { title: 'Hello - Please Login To Your Account' });
         } else {
-            // attempt automatic login //
-            AM.autoLogin(req.cookies.user, req.cookies.pass, function(o) {
-                if (o != null) {
-                    req.session.user = o;
-                    res.redirect('/home');
-                } else {
-                    res.render('login', { title: 'Hello - Please Login To Your Account' });
-                }
-            });
+            res.redirect('/home');
         }
     });
 
