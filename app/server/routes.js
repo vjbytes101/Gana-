f9843162bc653f1474e16e27390d291625f696c7
Inv1cts <hs3499@nyu.edu>
2017-10-03 21:56:00 -0400

    basic structure ready

---
+++ app/server/routes.js
@@ -0,0 +1,109 @@
+var AM = require('./modules/account-manager');
+
+module.exports = function(app) {
+
+// main login page //
+	app.get('/', function(req, res){
+		// check if the user's credentials are saved in a cookie //
+		if (req.cookies.user == undefined || req.cookies.pass == undefined){
+			res.render('login', { title: 'Hello - Please Login To Your Account' });
+		}	else{
+		// attempt automatic login //
+			AM.autoLogin(req.cookies.user, req.cookies.pass, function(o){
+				if (o != null){
+				    req.session.user = o;
+					res.redirect('/home');
+				}	else{
+					res.render('login', { title: 'Hello - Please Login To Your Account' });
+				}
+			});
+		}
+	});
+	
+	app.post('/', function(req, res){
+		AM.manualLogin(req.body['user'], req.body['pass'], function(e, o){
+			if (!o){
+				res.status(400).send(e);
+			}	else{
+				req.session.user = o;
+				if (req.body['remember-me'] == 'true'){
+					res.cookie('user', o.user, { maxAge: 900000 });
+					res.cookie('pass', o.pass, { maxAge: 900000 });
+				}
+				res.status(200).send(o);
+			}
+		});
+	});
+	
+// logged-in user homepage //
+	
+	app.get('/home', function(req, res) {
+		if (req.session.user == null){
+		// if user is not logged-in redirect back to login page //
+			res.redirect('/');
+		}	else{
+			res.render('home', {
+				title : 'Control Panel',
+				countries : CT,
+				udata : req.session.user
+			});
+		}
+	});
+	
+	app.post('/home', function(req, res){
+		if (req.session.user == null){
+			res.redirect('/');
+		}	else{
+			AM.updateAccount({
+				id		: req.session.user._id,
+				name	: req.body['name'],
+				email	: req.body['email'],
+				pass	: req.body['pass'],
+				country	: req.body['country']
+			}, function(e, o){
+				if (e){
+					res.status(400).send('error-updating-account');
+				}	else{
+					req.session.user = o;
+			// update the user's login cookies if they exists //
+					if (req.cookies.user != undefined && req.cookies.pass != undefined){
+						res.cookie('user', o.user, { maxAge: 900000 });
+						res.cookie('pass', o.pass, { maxAge: 900000 });	
+					}
+					res.status(200).send('ok');
+				}
+			});
+		}
+	});
+
+	app.post('/logout', function(req, res){
+		res.clearCookie('user');
+		res.clearCookie('pass');
+		req.session.destroy(function(e){ res.status(200).send('ok'); });
+	})
+	
+	// creating new accounts //
+	
+	app.get('/signup', function(req, res) {
+		res.render('signup', {  title: 'Signup', countries : CT });
+	});
+	
+	app.post('/signup', function(req, res){
+		AM.addNewAccount({
+			name 	: req.body['name'],
+			email 	: req.body['email'],
+			user 	: req.body['user'],
+			pass	: req.body['pass'],
+			country : req.body['country']
+		}, function(e){
+			if (e){
+				res.status(400).send(e);
+			}	else{
+				res.status(200).send('ok');
+			}
+		});
+	});
+
+	app.get('*', function(req, res) { res.render('404', { title: 'Page Not Found'}); });
+
+};
